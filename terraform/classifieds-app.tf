eb3ce4b817ff28e8fbf7fef2ccff6dc100c82264
Brian Yingling <brian@jet.com>
2020-01-23 22:33:25 -0500

    wrapping up deployment

---
+++ terraform/classifieds-app.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "classifieds-app" {
+    bucket = "byingling-microservices-demo-classifieds-app"
+    acl = "public-read"
+    policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicRead",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": ["s3:GetObject"],
+            "Resource": ["arn:aws:s3:::byingling-microservices-demo-classifieds-app/*"]
+        }
+    ]    
+}
+
+POLICY
+
+    website {
+        index_document = "index.html"
+    }
+}
